ef73f44a3ae5d76ddf7f696dd26c27068e4a129e
MahamudunnobyBadhon <badhondzs15cpscr17@gmail.com>
2022-01-01 01:28:19 +0600

    confirm

--- src/modules/ConfirmAppoinment/components/AppoinmentDate/AppoinmentDate.js
+++ src/modules/ConfirmAppoinment/components/AppoinmentDate/AppoinmentDate.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import {Box, Text} from "@chakra-ui/react"
+import {Box, Text, Flex} from "@chakra-ui/react"
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { Calendar } from "react-modern-calendar-datepicker";
 const AppoinmentDate  = () => {
    
     const [selectedDay, setSelectedDay] = useState(null);
   return (
-    <Box width="80" mx="auto">
-        <Box width="60%" mx="auto">
+    <Flex justifyContent="center">
+        <Box>
             <Text fontSize="20px" fontFamily="Asap" fontWeight="600">Pick Up A Date</Text>
         <Calendar
       value={selectedDay}
@@ -15,7 +15,7 @@ const AppoinmentDate  = () => {
       shouldHighlightWeekends
     />
     </Box>
-    </Box>
+    </Flex>
   );
       
 }
--- src/modules/ConfirmAppoinment/components/AppoinmentTime/AppoinmentTime.js
+++ src/modules/ConfirmAppoinment/components/AppoinmentTime/AppoinmentTime.js
@@ -1,6 +1,109 @@
-import {Box} from "@chakra-ui/react"
-export default function AppoinmentTime(){
-    return(
-        <Box></Box>
-    )
+import {Box, SimpleGrid, Flex, Text, Button} from '@chakra-ui/react';
+export default function AppoinmentTime() {
+  return (
+    <Box width="60%" mx="auto">
+      <Flex justifyContent="center">
+        <Box marginTop="30px" width="35%" mx="auto">
+          <Text fontWeight="600" fontFamily="Asap" fontSize="20px">
+            Select A Time
+          </Text>
+          <SimpleGrid columns={4} gap={4}>
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+            >
+              8.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+              isDisabled
+            >
+              9.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+            >
+              10.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+            >
+              11.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+              isDisabled
+            >
+              12.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+            >
+              13.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+              isDisabled
+            >
+              14.00
+            </Button>
+
+            <Button
+              height="50px"
+              width="80px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+            >
+              15.00
+            </Button>
+          </SimpleGrid>
+
+          <Button
+          marginTop="40px"
+              height="60px"
+              width="335px"
+              colorScheme="teal"
+              bg="#504DE5"
+              size="lg"
+            >
+             Confirm
+            </Button>
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
